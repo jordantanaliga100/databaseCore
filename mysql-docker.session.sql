bc1d8947047a172ea1348926d91672d4459d6edf
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-06-21 20:23:27 +0800

    feat(exercise): creating, inserting, altering tables and columns

--- mysql-docker.session.sql
+++ mysql-docker.session.sql
@@ -1,6 +1,50 @@
 
 -- ! SELECT VERSION 
-SELECT version();
+-- SELECT version();
+
+-- ! Task 1: create a database 
+-- CREATE DATABASE online_shop; --mysql
+
+-- ! Task 2 + Task 3: create a table
+-- CREATE TABLE IF NOT EXISTS products (
+--   name VARCHAR(200),
+--   price NUMERIC(10,2),
+--   description TEXT,
+--   amount_in_stock SMALLINT,
+--   image_path VARCHAR(500) 
+-- )
+
+-- ! Task 4: inserting a dummy data
+
+-- INSERT INTO products 
+-- VALUES(
+--   'Guitar',
+--   12.99,
+--   'This is a guitar',
+--   5,
+--   'uploads/products/123/pic.jpeg'
+
+-- )
+
+-- ! Task 5: add some constraints that make sense --mysql
+  -- step 1: Modify column types and NOT NULL constraints
+-- ALTER TABLE products
+--   MODIFY COLUMN name VARCHAR(200) NOT NULL,
+--   MODIFY COLUMN price NUMERIC(10,2) NOT NULL,
+--   MODIFY COLUMN description TEXT NOT NULL,
+--   MODIFY COLUMN amount_in_stock SMALLINT ;
+
+  -- Step 2: Add CHECK constraints
+-- ALTER TABLE products
+--   ADD CONSTRAINT price_positive CHECK (price > 0),
+--   ADD CONSTRAINT amount_positive_or_zero CHECK(amount_in_stock >= 0);
+
+
+
+
+
+
+
 
 -- ! selecting all records
 -- SELECT * FROM users;
