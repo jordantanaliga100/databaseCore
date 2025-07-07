da825ac365fb13cae521691aa0ca9c12189aa0b1
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-07-08 04:39:16 +0800

    chore(add-aggregations-2): added group by in comb with SUM,AVG,ROUND and added COALESCE

--- mysql-docker.session.sql
+++ mysql-docker.session.sql
@@ -47,3 +47,39 @@ SELECT MAX(b.num_guests) AS number_of_guests, MAX(num_seats) AS number_of_seats
     INNER JOIN tables AS t ON b.table_id = t.id
     INNER JOIN payment_methods AS p ON b.payment_id = p.id 
       WHERE t.num_seats < 5 AND p.name = 'Cash';
+
+-- * GROUP BY
+SELECT booking_date, SUM(num_guests) AS total_guests 
+  FROM bookings 
+    GROUP BY booking_date;
+
+SELECT booking_date, SUM(num_guests) AS total_guests 
+  FROM bookings 
+    INNER JOIN payment_methods AS p ON p.name = 'Cash'
+    GROUP BY booking_date
+
+SELECT booking_date, SUM(num_guests) AS total_guests, payment_methods.name  AS payment_method,
+SUM(amount_billed) AS higher_than_20
+FROM bookings 
+INNER JOIN payment_methods ON payment_methods.id = bookings.payment_id
+WHERE amount_billed > 100.00
+GROUP BY booking_date, payment_methods.name
+;
+
+SELECT *
+FROM payment_methods AS p 
+  INNER JOIN bookings AS b ON p.id = b.payment_id
+  INNER JOIN tables AS t ON b.table_id = t.id
+  ;
+
+SELECT 
+    b.booking_date,
+    p.name AS payment_method,
+    t.category AS table_size,
+    COUNT(*) AS total_bookings,
+   COALESCE(ROUND(AVG(amount_tipped), 2),0) AS tipped
+FROM payment_methods AS p
+INNER JOIN bookings AS b ON p.id = b.payment_id
+INNER JOIN tables AS t ON b.table_id = t.id
+GROUP BY b.booking_date, p.name, t.category
+ORDER BY booking_date;
