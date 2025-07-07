4bf50662d8973c4e43f5dbf1309eda7e4be2ab86
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-07-08 05:44:48 +0800

    chore(window-functions): add OVER and RANK window functions

--- postgres-docker.session.sql
+++ postgres-docker.session.sql
@@ -147,5 +147,13 @@ HAVING SUM(amount_billed) =  (
 )
 
 
+-- * WINDOW FUNCTIONS
+SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER() FROM bookings;
 
+SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER(PARTITION BY booking_date) FROM bookings;
 
+SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER(PARTITION BY booking_date ORDER BY amount_billed) 
+FROM bookings;
+
+SELECT booking_date, amount_tipped, RANK() OVER(PARTITION BY booking_date ORDER BY amount_tipped DESC) 
+FROM bookings;
